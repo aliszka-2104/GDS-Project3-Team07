46112567c28ca4bb69b12b129f8c60bfbc4bb061
aliszka <agata.anna.debiec@gmail.com>
2020-04-26 01:49:31 +0200

    Add basic movement

---
+++ src/The Forest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Rigidbody2D rigidbody;
+    private PlayerIsometricDirection playerIsometricDirection;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        playerIsometricDirection = GetComponent<PlayerIsometricDirection>();
+    }
+
+    private void FixedUpdate()
+    {
+        var currentPosition = rigidbody.position;
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var input = new Vector2(horizontal, vertical);
+        input = Vector2.ClampMagnitude(input, 1);
+        var movement = input * speed;
+        var newPosition = currentPosition + movement * Time.deltaTime;
+
+        rigidbody.MovePosition(newPosition);
+        playerIsometricDirection.SetDirection(movement);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
