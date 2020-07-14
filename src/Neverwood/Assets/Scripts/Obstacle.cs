f506e887b4d4f46e3a15fb68ad5aa363cd16f1ab
aliszka <agata.anna.debiec@gmail.com>
2020-07-14 19:48:48 +0200

    Add obstacle

---
+++ src/Neverwood/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public enum OBSTACLE
+    {
+        ROCK,
+        WOOD
+    }
+
+    public Transform target;
+    public OBSTACLE obstacleType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.GetComponent<PlayerMovement>()) return;
+
+        other.GetComponent<PlayerMovement>().CrossObstacle(target.position, obstacleType);
+    }
+}
