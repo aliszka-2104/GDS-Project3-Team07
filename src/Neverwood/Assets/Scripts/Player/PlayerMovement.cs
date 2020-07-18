e7ffd83fd5dd448d7ed2aecddd77cedfb4060334
aliszka <agata.anna.debiec@gmail.com>
2020-07-18 21:27:34 +0200

    Revert "Merge branch 'obstacles' into develop"
    This reverts commit 43807cf9432561e565b63887122936d4424a52cc, reversing
    changes made to 02eb72b4eca9241d9f294b1d5803fdcdb6d4b43a.

--- src/Neverwood/Assets/Scripts/Player/PlayerMovement.cs
+++ src/Neverwood/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,11 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Obstacle;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public List<OBSTACLE> obstaclesICanCross=new List<OBSTACLE>();
     public float speed = 5f;
     public GameObject light;
 
@@ -15,9 +13,6 @@ public class PlayerMovement : MonoBehaviour
     private PlayerDirection playerDirection;
     private Vector3 input = Vector3.zero;
     private Vector3 moveDirection;
-
-    public bool movingTo = false;
-    public Vector3 myTarget;
 
     void Awake()
     {
@@ -28,70 +23,16 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.E))
         {
             light.SetActive(!light.activeSelf);
         }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            var colliders = Physics.OverlapSphere(transform.position, 2f, LayerMask.GetMask("Campfire"));
-            
-            if (colliders.Length>0)
-            {
-                var first = colliders[0];
 
-                if (first.GetComponent<Campfire>())
-                {
-                    first.GetComponent<Campfire>().ToggleLight();
-                }
-            }
-        }
-        if (movingTo)
-        {
-            Teleport(myTarget);
-        }
-        else
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-            cc.Move(moveDirection * Time.deltaTime);
-            transform.rotation = Quaternion.identity;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+        cc.Move(moveDirection * Time.deltaTime);
+        transform.rotation = Quaternion.identity;
 
-            playerDirection.SetDirection(moveDirection);
-        }
-    }
-
-    public void CrossObstacle(Vector3 target, OBSTACLE obstacleType)
-    {
-        if (!obstaclesICanCross.Contains(obstacleType)) return;
-        if (movingTo) return;
-        movingTo = true;
-        myTarget = new Vector3(target.x, 0, target.z);
-        //Teleport(myTarget);
-    }
-
-    void Teleport(Vector3 target)
-    {
-        cc.enabled = false;
-        transform.position = target;
-
-        cc.enabled = true;
-        movingTo = false;
-    }
-
-    void MoveTowardsTarget(Vector3 target)
-    {
-        var offset = target - transform.position;
-        //collider.enabled = false;
-        if (offset.magnitude > .1f)
-        {
-            offset = offset.normalized * speed;
-            cc.Move(offset * Time.deltaTime);
-        }
-        else
-        {
-            movingTo = false;
-        //collider.enabled = true;
-        }
+        playerDirection.SetDirection(moveDirection);
     }
 }
