09328cf520f83be47a1c349b55c63db8ff269dcf
aliszka <agata.anna.debiec@gmail.com>
2020-07-28 22:19:15 +0200

    Create CharacterSwitcher.cs

---
+++ src/Neverwood/Assets/Scripts/Player/CharacterSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitcher : MonoBehaviour
+{
+    private PlayerMovement[] characters;
+    private int currentCharacter = 0;
+
+    void Start()
+    {
+        characters = FindObjectsOfType<PlayerMovement>();
+        characters[currentCharacter].SetCurrentCharacter(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchCharacter();
+        }
+    }
+
+    private void SwitchCharacter()
+    {
+        characters[currentCharacter].SetCurrentCharacter(false);
+
+        currentCharacter++;
+        currentCharacter %= characters.Length;
+
+        characters[currentCharacter].SetCurrentCharacter(true);
+    }
+}
