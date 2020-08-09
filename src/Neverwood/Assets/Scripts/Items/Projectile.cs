baafa6c682f4319faa0cdb4d425dc06b34438e6a
aliszka <agata.anna.debiec@gmail.com>
2020-08-09 21:27:59 +0200

    Add attack

--- src/Neverwood/Assets/Scripts/Items/Projectile.cs
+++ src/Neverwood/Assets/Scripts/Items/Projectile.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Transforms to act as start and end markers for the journey.
+    public Vector3 start;
+    public Vector3 target;
+
+    public GameObject audioCue;
+
+    // Movement speed in units per second.
+    public float speed = 1.0F;
+
+    private bool moving = false;
+
     void Start()
     {
-        
+        start = transform.position;
     }
 
-    // Update is called once per frame
+    // Move to the target end position.
     void Update()
     {
-        
+        if (!moving) return;
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+
+    internal void GoToTarget(Vector3 target)
+    {
+        //this.target = new Vector3(target.x,0,target.z);
+        this.target = target;
+        moving = true;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer==LayerMask.NameToLayer("Ground"))Instantiate(audioCue, transform.position, Quaternion.identity);
+
+        Debug.Log("Hit with "+collision.gameObject.name);
+        Destroy(gameObject);
     }
 }
---
+++ src/Neverwood/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject projectile;
+
+    private Player player;
+    private GameObject selectedObject;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+    
+    void Update()
+    {
+        if (!player.IsCurentCharacter) return;
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        //Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hitData;
+
+        //if (Physics.Raycast(ray, out hitData, 1000))
+        //{
+        //    worldPosition = hitData.point;
+        //    Shoot(worldPosition);
+        //}
+
+        Shoot(GeteMousePos(transform.position));
+    }
+
+    void Shoot(Vector3 target)
+    {
+        var projectileSpawn = transform.position + 2*Vector3.up;
+        //var obj = Instantiate(projectile, target, Quaternion.identity);
+        var obj = Instantiate(projectile, projectileSpawn, Quaternion.identity);
+        if(obj.GetComponent<Projectile>()!=null)
+        {
+            obj.GetComponent<Projectile>().GoToTarget(target);
+        }
+    }
+
+    public Vector3 GetPlayerPlaneMousePos(Vector3 aPlayerPos)
+    {
+        Plane plane = new Plane(Vector3.up, aPlayerPos);
+        Ray  ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float dist;
+        if (plane.Raycast(ray, out dist))
+        {
+            return ray.GetPoint(dist);
+        }
+        return Vector3.zero;
+    }
+
+    public Vector3 GeteMousePos(Vector3 aPlayerPos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float dist;
+        RaycastHit hit;
+        if (Physics.Raycast(ray,out hit,1000,LayerMask.GetMask("Ground","NPC")))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+}
